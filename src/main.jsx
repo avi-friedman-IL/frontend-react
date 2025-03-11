6e236ef8b83f8a32f8b51904f6cdf3684ada4947
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2025-03-11 11:58:58 +0200

    Remove service worker files and update registration logic

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from "react-redux"
 
-import * as serviceWorkerRegistration from '../public/serviceWorkerRegistration'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './i18n'
 
 import { store } from './store/store'
@@ -19,7 +19,16 @@ root.render(
 	</Provider>
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+// Register service worker with proper error handling
+if (process.env.NODE_ENV === 'production') {
+	serviceWorkerRegistration.register({
+		onSuccess: (registration) => {
+			console.log('Service Worker registered successfully:', registration)
+		},
+		onUpdate: (registration) => {
+			console.log('New content is available; please refresh.', registration)
+		}
+	})
+} else {
+	serviceWorkerRegistration.unregister()
+}
