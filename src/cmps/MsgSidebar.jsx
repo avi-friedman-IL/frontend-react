191fee3c421d3161db6182cd857ed4de09e962f7
avi-friedman-IL <155029199+avi-friedman-IL@users.noreply.github.com>
2025-05-25 16:05:33 +0300

    Integrate template loading in MsgSidebar: Replace subjects with templates from the Redux store, and implement useEffect to load templates on component mount for improved message filtering functionality.

--- src/cmps/MsgSidebar.jsx
+++ src/cmps/MsgSidebar.jsx
@@ -2,14 +2,20 @@ import { setFilter } from '../store/actions/msg.actions'
 import { useSelector } from 'react-redux'
 import { t } from 'i18next'
 import { msgService } from '../services/msg'
+import { loadTemplates } from '../store/actions/template.actions'
+import { useEffect } from 'react'
 
 export function MsgSidebar() {
    const filterBy = useSelector(state => state.msgModule.filterBy)
-   const subjects = msgService.getSubjects()
+   const templates = useSelector(state => state.templateModule.templates)
    function handleClick(ev, subject) {
       ev.preventDefault()
       setFilter({ ...filterBy, subject })
    }
+
+   useEffect(() => {
+      loadTemplates()
+   }, [])
    return (
       <ul className='msg-sidebar'>
          <li
@@ -18,14 +24,14 @@ export function MsgSidebar() {
             className={`subject-filter ${!filterBy.subject ? 'active' : ''}`}>
             <button>{t('all')}</button>
          </li>
-         {subjects.map((subject, idx) => (
+         {templates?.map((template) => (
             <li
-               key={subject.id}
-               onClick={ev => handleClick(ev, subject.title)}
+               key={template._id}
+               onClick={ev => handleClick(ev, template.title)}
                className={`subject-filter ${
-                  subject.title === filterBy.subject ? 'active' : ''
+                  template.title === filterBy.subject ? 'active' : ''
                }`}>
-               <button>{t(subject.title)}</button>
+               <button>{t(template.title)}</button>
             </li>
          ))}
       </ul>
